fbe869bcca575b77471244ca3db0ce0678a8b3e6
Aron Martinez <martinezar@lesd.k12.az.us>
2016-09-20 14:34:47 -0700

    Update readme and add PDO for DB

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # soNerd starter kit #
 
-### Visit [sonerd.com](http://sonerd.com) for more info ###
+### Visit [sonerd.com](http://sonerd.com) - need to update link - for more info ###
+
+### Quick Explanation ###
+* A starting point for the next project using PHP
+* [SCSS](http://sass-lang.com/guide) , [JQuery](https://jquery.com/) , [Bootstrap](http://getbootstrap.com/)
+* MySQL DB connections
+* Uses a configuration to define paths and configure some settings
+* Header and Footer files in order to have same header and footer across separate pages
 
 ### Contact ###
 * Aron Martinez - aron@sonerd.com
-
-### Quick Explanation ###
-A starting point for the next project using PHP
--- config/db-connection.php
+++ config/db-connection.php
@@ -1,24 +1,34 @@
 <?php
 
 // Configuration file for connecting to a MySQL Database
-define("DB_HOST", "localhost");
+define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PW", "password");
+define("DB_DRIVER", "mysql");
 
 // Define table or tables if using more than one
-define("DB_1", "db_1");
-define("DB_2", "db_2");
+define("DB_DB1", "db_1");
+define("DB_DB2", "db_2");
+define("DB_DB3", "db_3");
 
 // Connect to database
 // Procedurual method
-$db_conn_1 = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_1) or die("DB1 connection failed: " . mysqli_error($db_conn_1));
+$db_conn_1 = mysqli_connect(DB_SERVER, DB_USER, DB_PW, DB_DB1) or die("DB1 connection failed: " . mysqli_error($db_conn_1));
 if(mysqli_connect_errno($db_conn_1)) {
     trigger_error('DB1 connection failed: ' . mysqli_error($db_conn_1), E_USER_ERROR);
 }
 // OOP method
-$db_conn_2 = new mysqli(DB_HOST, DB_USER, DB_PW, DB_2);
+$db_conn_2 = new mysqli(DB_SERVER, DB_USER, DB_PW, DB_DB2);
 if($db_conn_2->connect_error) {
     trigger_error('DB2 connection failed: ' . $db_conn_2->connect_error, E_USER_ERROR);
+}
+// PDO
+try {
+    $db = new PDO(DB_DRIVER . ":dbname=" . DB_DB3 . ";host=" . DB_SERVER, DB_USER, DB_PW);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMDE_EXCEPTION);
+
+} catch(PDOException $e) {
+    trigger_error('Error: ' . $e->getMessage(), E_USER_ERROR);
 }
 
 // Define tables if desired
