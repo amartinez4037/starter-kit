a85cd1bdab2acad3984a514c12fc570f18625db3
Aron Martinez <martinezar@lesd.k12.az.us>
2016-09-20 11:17:11 -0700

    Fix CSS no quotes

--- config.php
+++ config.php
@@ -10,13 +10,13 @@
 	// Set the time zone for PHP
 	// date_default_timezone_set('America/Phoenix');
 
-	// Define home page
-	defined("HOME") or define("HOME", "http://sonerd.com/");
+	// Define links
+	defined("HOME") or define("HOME", "http://10.13.1.51/");
+	defined("CSS") or define("CSS", HOME . "/assets/css/main.css");
 
 	//	Folder paths
 	// realpath(dirname(__FILE__)) => gets path to this specific file
 	defined("LIB_PATH") or define("LIB_PATH", realpath(dirname(__FILE__) . '/assets/lib'));
     defined("HEADER") or define("HEADER", realpath(dirname(__FILE__) . '/assets/templates/header.php'));
 	defined("FOOTER") or define("FOOTER", realpath(dirname(__FILE__) . '/assets/templates/footer.php'));
-	defined("CSS") or define("CSS", realpath(dirname(__FILE__) . '/assets/css/main.css'));
 ?>
