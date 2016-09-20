e64d9f3f5ef0fcafa20c7f30a2175b6a503201e0
Aron Martinez <martinezar@lesd.k12.az.us>
2016-09-20 11:02:26 -0700

    Add CSS

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -2,13 +2,13 @@
 <html lang="en">
     <head>
     	<meta charset='UTF-8' />
-    	<meta name='description' content='Starter Kit for an application' />
+    	<meta name='description' content='Starter Kit for a new application' />
     	<meta name='author' content='Aron Martinez' />
     	<meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Starter Kit</title>
 
         <!-- <link rel="shortcut icon" href="assets/img/favicon.ico"> -->
-        <!-- <link rel='stylesheet' type='text/css' href='assets/css/main.css' /> -->
+        <link rel='stylesheet' type='text/css' href=<?php echo CSS; ?> />
 
         <!-- Bootstrap -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" > -->
--- config.php
+++ config.php
@@ -11,12 +11,12 @@
 	// date_default_timezone_set('America/Phoenix');
 
 	// Define home page
-	defined("APP_PATH") or define("APP_PATH", "http://sonerd.com/");
+	defined("HOME") or define("HOME", "http://sonerd.com/");
 
 	//	Folder paths
 	// realpath(dirname(__FILE__)) => gets path to this specific file
-    defined("CSS_PATH") or define("CSS_PATH", realpath(dirname(__FILE__) . '/assets/css'));
 	defined("LIB_PATH") or define("LIB_PATH", realpath(dirname(__FILE__) . '/assets/lib'));
     defined("HEADER") or define("HEADER", realpath(dirname(__FILE__) . '/assets/templates/header.php'));
 	defined("FOOTER") or define("FOOTER", realpath(dirname(__FILE__) . '/assets/templates/footer.php'));
+	defined("CSS") or define("CSS", realpath(dirname(__FILE__) . '/assets/css/main.css'));
 ?>
