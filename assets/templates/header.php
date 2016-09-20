a6af50faa06388263f600adba3d282add28cdf79
Aron Martinez <martinezar@lesd.k12.az.us>
2016-09-20 15:04:07 -0700

    PHP in header navigation

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -23,8 +23,8 @@
     <body>
     	<header>
             <nav>
-                <a href="index.php">Home</a>
-                <a href="about.php">About</a>
+                <a href="<?php echo HOME; ?>">Home</a>
+                <a href="<?php echo HOME . 'about.php'; ?>">About</a>
             </nav>
         </header>
 
